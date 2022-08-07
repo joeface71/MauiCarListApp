cc7e96cc367f2dbf8f38fc8659f119c49c4a1ab6
joeface71 <joefacenda@yahoo.com>
2022-08-07 18:31:21 -0400

    Starting MVVM

---
+++ CarListApp.Maui/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CarListApp.Maui.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ CarListApp.Maui/Models/CarMart.cs
@@ -0,0 +1,8 @@
+﻿namespace CarListApp.Maui.Models
+{
+    public class CarMart
+    {
+        public int Id { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
